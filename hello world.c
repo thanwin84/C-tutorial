334f5d7f7398cdf4250a7c7959097473dbf5d969
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-05-06 17:46:32 +0600

    Create hello world.c

---
+++ hello world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("This is basic tutorial\n");
+    printf("This is the last message. \n");
+    return 0;
+}
